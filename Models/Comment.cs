c8d1ca1d9b5ab9cf51e9b3dd6d556bcbba90d332
Gregory Markus <gregmarkus1992@yahoo.com>
2024-04-22 20:34:51 -0500

    created the models

--- Models/Comment.cs
+++ Models/Comment.cs
@@ -2,5 +2,9 @@
 {
     public class Comment
     {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int BookId { get; set; }
+        public string Content { get; set; }
     }
 }
