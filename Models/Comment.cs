c2c31a8e7297b77177dd7654e4d44e1136fff82f
B33blebroxx <b33blebroxx@gmail.com>
2024-04-23 19:08:06 -0500

    added dateposted to comments class and dbcontext

--- Models/Comment.cs
+++ Models/Comment.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public int UserId { get; set; }
         public int BookId { get; set; }
-        public string Content { get; set; }
+        public string? Content { get; set; }
+        public DateTime? DatePosted { get; set; }
     }
 }
