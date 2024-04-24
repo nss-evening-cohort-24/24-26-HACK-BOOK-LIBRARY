f5d687e32607129d3cf369e073c50a74a2df9b6b
B33blebroxx <b33blebroxx@gmail.com>
2024-04-23 21:24:34 -0500

    added user apis, fixed migration issues, removed bookid from author model and dbcontext

---
+++ Migrations/20240424021838_dateupdate.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _24HackBookLibrary.Migrations
+{
+    public partial class dateupdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Comments",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DatePosted",
+                value: new DateTime(2024, 1, 10, 0, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                table: "Comments",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "DatePosted",
+                value: new DateTime(2024, 1, 15, 0, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                table: "Comments",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "DatePosted",
+                value: new DateTime(2024, 1, 22, 0, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                table: "Comments",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "DatePosted",
+                value: new DateTime(2024, 2, 1, 0, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                table: "Comments",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "DatePosted",
+                value: new DateTime(2024, 2, 12, 0, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                table: "Comments",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "DatePosted",
+                value: new DateTime(2024, 2, 23, 0, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                table: "Comments",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "DatePosted",
+                value: new DateTime(2024, 3, 11, 0, 0, 0, 0, DateTimeKind.Utc));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Comments",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DatePosted",
+                value: new DateTime(2024, 3, 5, 0, 2, 48, 848, DateTimeKind.Utc).AddTicks(1170));
+
+            migrationBuilder.UpdateData(
+                table: "Comments",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "DatePosted",
+                value: new DateTime(2024, 4, 3, 0, 2, 48, 848, DateTimeKind.Utc).AddTicks(1177));
+
+            migrationBuilder.UpdateData(
+                table: "Comments",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "DatePosted",
+                value: new DateTime(2024, 2, 22, 0, 2, 48, 848, DateTimeKind.Utc).AddTicks(1178));
+
+            migrationBuilder.UpdateData(
+                table: "Comments",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "DatePosted",
+                value: new DateTime(2024, 3, 12, 0, 2, 48, 848, DateTimeKind.Utc).AddTicks(1179));
+
+            migrationBuilder.UpdateData(
+                table: "Comments",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "DatePosted",
+                value: new DateTime(2024, 3, 13, 0, 2, 48, 848, DateTimeKind.Utc).AddTicks(1180));
+
+            migrationBuilder.UpdateData(
+                table: "Comments",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "DatePosted",
+                value: new DateTime(2024, 4, 14, 0, 2, 48, 848, DateTimeKind.Utc).AddTicks(1181));
+
+            migrationBuilder.UpdateData(
+                table: "Comments",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "DatePosted",
+                value: new DateTime(2024, 4, 22, 0, 2, 48, 848, DateTimeKind.Utc).AddTicks(1182));
+        }
+    }
+}
---
+++ Migrations/20240424022337_authorUpdate.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _24HackBookLibrary.Migrations
+{
+    public partial class authorUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BookId",
+                table: "Authors");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "BookId",
+                table: "Authors",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "BookId",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "BookId",
+                value: 2);
+
+            migrationBuilder.UpdateData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "BookId",
+                value: 3);
+
+            migrationBuilder.UpdateData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "BookId",
+                value: 4);
+
+            migrationBuilder.UpdateData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "BookId",
+                value: 5);
+
+            migrationBuilder.UpdateData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "BookId",
+                value: 6);
+
+            migrationBuilder.UpdateData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "BookId",
+                value: 7);
+        }
+    }
+}
