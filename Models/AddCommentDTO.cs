8e27c3b2ad4a9be18b586eb18cd67c0642244df4
Nathan <nathopp@gmail.com>
2024-04-29 18:24:17 -0500

    Added Comments DTO

---
+++ Models/AddCommentDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace _24HackBookLibrary.Models
+{
+    public class AddCommentDTO
+    {
+        public int UserId { get; set; }
+        public int BookId { get; set; }
+        public string? Content { get; set; }
+    }
+}
