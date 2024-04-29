415702d8adbf7a56e81a3937cfbf90c5c6b6c79d
B33blebroxx <b33blebroxx@gmail.com>
2024-04-29 18:56:56 -0500

    Get single genre by id.

--- API/GenreAPI.cs
+++ API/GenreAPI.cs
@@ -8,6 +8,18 @@
             {
                 return db.Genres.ToList();
             });
+
+            //Get single genre by id
+            app.MapGet("/genres/{id}", (_24HackBookLibraryDbContext db, int id) =>
+            {
+                var genre = db.Genres.FirstOrDefault(g => g.Id == id);
+                if (genre == null)
+                {
+                    return Results.NotFound("Genre not found");
+                }
+
+                return Results.Ok(genre);
+            });
         }
     }
 }
