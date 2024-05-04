3bc9f27792a3d21e64921415784ab64f11ba61a6
B33blebroxx <b33blebroxx@gmail.com>
2024-05-04 13:10:27 -0500

    Created sort within all books call, added bookcover to search return, emptied string in results.notfound in error catch conditional in search call.

--- Models/UserBookRating.cs
+++ Models/UserBookRating.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public int UserId { get; set; }
         public int BookId { get; set; }
-        public int Score { get; set; }
+        public double Score { get; set; }
 
         public User? User { get; set; }
         public Book? Book { get; set; }
